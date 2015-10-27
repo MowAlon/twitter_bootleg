f2d2fcd34c436439c2437a682e9c2ed251a0e4d9
Alon Waisman <mowalon@yahoo.com>
2015-10-27 11:30:48 -0600

    Heroku debugging 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
 
   def create
+    user = User.from_omniauth(request.env["omniauth.auth"])
     # if user = User.from_omniauth(request.env["omniauth.auth"])
     #   session[:user_id] = user.id
     # end
     # redirect_to profile_path
-    render text: request.env["omniauth.auth"]
+    render text: user.attributes
   end
 
   def destroy
