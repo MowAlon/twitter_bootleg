385fd94a7566ba0d91f38ae7a35dffa2aa2a82e9
Alon Waisman <mowalon@yahoo.com>
2015-10-27 11:25:36 -0600

    testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_info)
-    where(uid: auth_info[:uid]).first_or_create do |new_user|
-      new_user.uid                = auth_info.uid
-      new_user.name               = auth_info.extra.raw_info.name
-      new_user.screen_name        = auth_info.extra.raw_info.screen_name
-      new_user.oauth_token        = auth_info.credentials.token
-      new_user.oauth_token_secret = auth_info.credentials.secret
-      new_user.profile_image      = auth_info.info.image
-      byebug
-    end
+    user = User.first_or_create(uid: auth_info.uid)
+    user.uid                = auth_info.uid
+    user.name               = auth_info.extra.raw_info.name
+    user.screen_name        = auth_info.extra.raw_info.screen_name
+    user.oauth_token        = auth_info.credentials.token
+    user.oauth_token_secret = auth_info.credentials.secret
+    user.profile_image      = auth_info.info.image
+    user.save
+    user
   end
 end
