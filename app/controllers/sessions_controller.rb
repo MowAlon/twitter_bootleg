9cf9fcf8f68a4ed7acee28763cc389d2c735aec8
Alon Waisman <mowalon@yahoo.com>
2015-10-27 11:27:30 -0600

    Heroku debugging 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     #   session[:user_id] = user.id
     # end
     # redirect_to profile_path
-    render text: "Hi, there!"
+    render text: request.env["omniauth.auth"]
   end
 
   def destroy
