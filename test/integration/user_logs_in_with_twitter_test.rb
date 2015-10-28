3e0fc81b5411acb255f14a1724aae8601b10cb23
Alon Waisman <mowalon@yahoo.com>
2015-10-28 15:17:20 -0600

    Crazy DB bug resolved by removing tweets.yml

--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -1,40 +1,40 @@
 require "test_helper"
 
 class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
-  # include Capybara::DSL
-  #
-  # def setup
-  #   Capybara.app = TwitterBootleg::Application
-  #   stub_omniauth
-  # end
-  #
-  # test "logging in" do
-  #   visit "/"
-  #   assert_equal 200, page.status_code
-  #   click_link "Login"
-  #   assert_equal "/", current_path
-  #   assert page.has_content?("Alon")
-  #   assert page.has_link?("Logout")
-  # end
-  #
-  # def stub_omniauth
-  #   # first, set OmniAuth to run in test mode
-  #   OmniAuth.config.test_mode = true
-  #   # then, provide a set of fake oauth data that
-  #   # omniauth will use when a user tries to authenticate:
-  #   OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
-  #     provider: 'twitter',
-  #     extra: {
-  #       raw_info: {
-  #         user_id: "1234",
-  #         name: "Alon",
-  #         screen_name: "MowAlon",
-  #       }
-  #     },
-  #     credentials: {
-  #       token: "TimTam",
-  #       secret: "TimTamSlam"
-  #     }
-  #   })
-  # end
+  include Capybara::DSL
+
+  def setup
+    Capybara.app = TwitterBootleg::Application
+    stub_omniauth
+  end
+
+  test "logging in" do
+    visit "/"
+    assert_equal 200, page.status_code
+    click_link "Login"
+    assert_equal "/profile", current_path
+    assert page.has_content?("Alon")
+    assert page.has_link?("Logout")
+  end
+
+  def stub_omniauth
+    # first, set OmniAuth to run in test mode
+    OmniAuth.config.test_mode = true
+    # then, provide a set of fake oauth data that
+    # omniauth will use when a user tries to authenticate:
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: 'twitter',
+      extra: {
+        raw_info: {
+          user_id: "1234",
+          name: "Alon",
+          screen_name: "MowAlon",
+        }
+      },
+      credentials: {
+        token: "TimTam",
+        secret: "TimTamSlam"
+      }
+    })
+  end
 end
--- test/models/twitter_test.rb
+++ test/models/twitter_test.rb
@@ -2,14 +2,12 @@ require 'test_helper'
 
 class TwitterTest < ActiveSupport::TestCase
 
-  test "testing crazy database bug" do
-  end
+  test "it can connect and retrieve info from Twitter - use #current_user" do
+    alon = User.find_by(name: "Alon Waisman")
+    screen_name = alon.screen_name
 
-  # test "it can connect and retrieve info from Twitter - use #current_user" do
-  #   alon = User.find_by(name: "Alon Waisman")
-  #   screen_name = alon.screen_name
-  #
-  #   assert_equal "Alon Waisman", alon.twitter.current_user.name
-  #   assert_equal "MowAlon", alon.twitter.current_user.screen_name
-  # end
+    assert_equal "Alon Waisman", alon.twitter.current_user.name
+    assert_equal "MowAlon", alon.twitter.current_user.screen_name
+  end
+  
 end
